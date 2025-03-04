393dea3de234ad606ddaf3a21b2440b5e278a71f
Charles Holtzkampf <charles.holtzkampf@gmail.com>
2025-03-04 12:50:19 +0000

    dockerfile update

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # Run the install script to download and configure everything
 NGROK_AUTHTOKEN="${NGROK_AUTHTOKEN}" PROVIDER_API_KEY="${PROVIDER_API_KEY}" \
-PROVIDER_TYPE="${PROVIDER_TYPE:-ollama}" OLLAMA_URL="${OLLAMA_URL:-http://localhost:11434}" \
+PROVIDER_TYPE="${PROVIDER_TYPE:-ollama}" LLM_URL="${LLM_URL:-http://localhost:11434}" \
 SERVER_PORT="${SERVER_PORT:-8080}" /app/install.sh
 
 # Keep container running
